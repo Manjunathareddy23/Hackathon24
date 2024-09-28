98ca48257ec11fa6469bfabf3074784e2dffdec4
Manjunathareddy23 <141892277+Manjunathareddy23@users.noreply.github.com>
2024-09-28 10:09:24 +0530

    certification_resources.py

---
+++ certification_resources.py
@@ -0,0 +1,85 @@
+#%%writefile certification_resources.py
+#Certification recommendations with links
+certification_recommendations = {
+    'python': [
+        ('Google IT Automation with Python', 'https://www.coursera.org/professional-certificates/google-it-automation'),
+        ('IBM Python for Data Science', 'https://www.coursera.org/learn/python-for-applied-data-science-ai')
+    ],
+    'java': [
+        ('Coursera Java Programming and Software Engineering Fundamentals', 'https://www.coursera.org/specializations/java-programming')
+    ],
+    'machine learning': [
+        ('Google Machine Learning Crash Course', 'https://developers.google.com/machine-learning/crash-course'),
+        ('Coursera Machine Learning by Stanford University', 'https://www.coursera.org/learn/machine-learning')
+    ],
+    'data science': [
+        ('IBM Data Science Professional Certificate', 'https://www.coursera.org/professional-certificates/ibm-data-science'),
+        ('Coursera Data Science Specialization', 'https://www.coursera.org/specializations/jhu-data-science')
+    ],
+    'deep learning': [
+        ('Coursera Deep Learning Specialization by Andrew Ng', 'https://www.coursera.org/specializations/deep-learning')
+    ],
+    'cloud computing': [
+        ('Google Cloud Professional Cloud Architect', 'https://cloud.google.com/certification/cloud-architect'),
+        ('AWS Certified Solutions Architect', 'https://aws.amazon.com/certification/certified-solutions-architect-associate/')
+    ],
+    'sql': [
+        ('Coursera SQL for Data Science', 'https://www.coursera.org/learn/sql-for-data-science'),
+        ('IBM Data Analyst Professional Certificate', 'https://www.coursera.org/professional-certificates/ibm-data-analyst')
+    ],
+    'project management': [
+        ('Google Project Management Certificate', 'https://grow.google/certificates/project-management/')
+    ],
+    'data analysis': [
+        ('IBM Data Analyst Professional Certificate', 'https://www.coursera.org/professional-certificates/ibm-data-analyst'),
+        ('Coursera Data Analysis and Visualization with Python', 'https://www.coursera.org/learn/data-analysis-with-python')
+    ],
+    'communication': [
+        ('Coursera Communication Skills for Engineers', 'https://www.coursera.org/learn/communication-skills-engineers')
+    ],
+    'react': [
+        ('Coursera Front-End Web Development with React', 'https://www.coursera.org/learn/front-end-react')
+    ],
+    'docker': [
+        ('Docker Masterclass on Udemy', 'https://www.udemy.com/course/docker-mastery/')
+    ],
+    'tensorflow': [
+        ('Coursera Introduction to TensorFlow for Artificial Intelligence', 'https://www.coursera.org/learn/introduction-tensorflow')
+    ],
+    'pandas': [
+        ('Coursera Data Analysis with Pandas', 'https://www.coursera.org/learn/data-analysis-pandas')
+    ],
+    'kubernetes': [
+        ('Google Kubernetes Engine Quickstart', 'https://cloud.google.com/kubernetes-engine/docs/quickstart')
+    ],
+    'aws': [
+        ('AWS Cloud Practitioner Essentials', 'https://aws.amazon.com/training/course-descriptions/cloud-practitioner-essentials/')
+    ],
+    'azure': [
+        ('Microsoft Azure Fundamentals', 'https://learn.microsoft.com/en-us/certifications/azure-fundamentals/')
+    ],
+    'nlp': [
+        ('Coursera Natural Language Processing Specialization', 'https://www.coursera.org/specializations/natural-language-processing')
+    ],
+    'statistics': [
+        ('Coursera Statistics with R Specialization', 'https://www.coursera.org/specializations/statistics')
+    ],
+    'redux': [
+        ('Coursera Front-End Web Development with React', 'https://www.coursera.org/learn/front-end-react')
+    ],
+    'typescript': [
+        ('Coursera TypeScript for Beginners', 'https://www.coursera.org/learn/typescript')
+    ],
+    'ui/ux': [
+        ('Google UX Design Certificate', 'https://grow.google/certificates/ux-design/')
+    ],
+    'responsive design': [
+        ('Coursera Responsive Web Design', 'https://www.coursera.org/learn/responsive-web-design')
+    ],
+    'devops': [
+        ('Coursera DevOps Specialization', 'https://www.coursera.org/specializations/devops')
+    ],
+    'ci/cd': [
+        ('Coursera Continuous Integration and Continuous Deployment', 'https://www.coursera.org/learn/continuous-integration')
+    ]
+}
